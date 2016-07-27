5a2815a61e641a196b09e8f7b850b023db14e204
Will Kahn-Greene <willkg@bluesock.org>
2016-07-27 11:48:45 -0400

    Fix twitter dotjs stuff
    Twitter added a CSP policy so the .css file doesn't work anymore. This
    also fixes a bug in the .js code I had on the first line (can't take a
    slice of nothing) and adds the promoted-tweet removing code to the .js
    file.

--- dotfiles/.js/twitter.com.js
+++ dotfiles/.js/twitter.com.js
@@ -1,15 +1,21 @@
-var garbage = Array.prototype.slice.call(
-  document.getElementsByClassName('dashboard')
-);
- 
+var garbage = new Array();
+
 garbage.push(
-  document.getElementsByClassName('trends-inner')[0],
+  // document.getElementsByClassName('dashboard')[0],
+  // document.getElementsByClassName('trends-inner')[0],
   document.getElementsByClassName('wtf-module')[0]
 );
- 
  
 for (var i = 0; i < garbage.length; i++) {
   garbage[i].remove();
 }
- 
-document.getElementById('timeline').style.float = "none";
+
+function fixThings() {
+  var garbage = document.getElementsByClassName('promoted-tweet');
+  for (var i=0; i < garbage.length; i++) {
+    garbage[i].remove();
+  }
+}
+
+window.setInterval(fixThings, 1000);
+fixThings();
