9141f504107e9c523874ea40f0708f078bf42f37
Will Kahn-Greene <willkg@bluesock.org>
2018-02-09 09:23:55 -0500

    Add python utils setup script

---
+++ setup_python_utils.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install pipenv
+pip install --user pipenv
+
+# Install pipsi
+curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
+
+# Use pipsi to install other things
+toinstall=( autopep8 aws bandit check-manifest docker-compose flake8 hashin piprot sphinx tox twine youtube-dl )
+for pkg in "${toinstall[@]}"
+do
+    pipsi install "${pkg}"
+done
